33086f83164148ae93847c79305c65facb97659c
vipin <vipinchandra555@gmail.com>
2021-01-15 12:14:59 +0530

    update some changes on sidebar

--- src/Component/Profile.js
+++ src/Component/Profile.js
@@ -1,11 +1,13 @@
 import React from "react";
-import user from "../Images/user.png";
+/* import user from "../Images/user.png";
+ */
+import vipin from "../Images/vipin.png";
 
 const Profile = () => {
   return (
     <div className="profile">
-      <img class="profile-img" src={user} alt="" />
-      <h1>VIPIN CHANDRA</h1>
+      <img class="profile-img" src={vipin} alt="" />
+      <h4>Vipin Chandra</h4>
     </div>
   );
 };
